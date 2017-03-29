9025df3c1622419514c2042dc56f96cb36a81c0d
CHJ Hartgerink <chjh@protonmail.com>
2017-03-29 23:22:46 +0200

    Update crossref.py, init oadoi.py

---
+++ bin/oadoi.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import re as r
+import requests
+
+dirs = [x[0] for x in os.walk('db/doi')]
+
+for dir in dirs:
+    doi = dir[7:]
+    if doi != '' or len(doi) > 7:
+        r = requests.get("http://api.oadoi.org/%s?email=info@libscie.org" % doi)
+        data = r.json()
+        filename = '%s/oadoi.json' % dir
+        new_file = open(filename, "w")
+        new_file.write(str(data))
+        new_file.close()
+            
+
+
+
