8b7251c53d06980cfdfc874c1ae305733fc3a798
CHJ Hartgerink <chjh@protonmail.com>
2017-05-28 23:57:54 +0200

    Update bin scripts

--- bin/oadoi.py
+++ bin/oadoi.py
@@ -10,15 +10,20 @@ dirs = [x[0] for x in os.walk('db/doi')]
 i = 1
 
 for dir in dirs:
-    if i % 200000 == 0:
+    if i % 500000 == 0:
         time.sleep(50000)
     doi = dir[7:]
     if doi != '' or len(doi) > 7:
-        r = requests.get("http://api.oadoi.org/%s?email=info@libscie.org" % doi)
-        data = r.json()
-        filename = '%s/oadoi.json' % dir
-        new_file = open(filename, "w")
-        new_file.write(str(data))
-        new_file.close()
-    print('Just parsed %d of %d dois through oaDOI' % (i, len(dirs)))
-    i += 1
+        try:
+            if not os.path.isfile('%s/oadoi.json' % dir):
+                r = requests.get("http://api.oadoi.org/%s?email=info@libscie.org" % doi)
+                data = r.json()
+                filename = '%s/oadoi.json' % dir
+                new_file = open(filename, "w")
+                new_file.write(str(data))
+                new_file.close()
+        except:
+            with open("db/tmp-oadoi", "a") as myfile:
+                myfile.write("%s" % doi)
+        print('Just parsed %d of %d dois through oaDOI' % (i, len(dirs)))
+        i += 1
--- bin/zenodo-access.py
+++ bin/zenodo-access.py
@@ -12,16 +12,27 @@ i = 1
 for dir in dirs:
     doi = dir[7:]
     if doi != '' or len(doi) > 7:
-        r = requests.get(
-    		'https://zenodo.org/api/records',
-    		params={'q': 'doi:"%s"' % dir})
-        data = r.json()
-        if data['hits']['total'] > 0:
-        	filename = '%s/zenodo-access.json' % dir
-        	new_file = open(filename, "w")
-        	new_file.write(str(data))
-        	new_file.close()
-        else if r.status_code != 200:
-            time.sleep(5000)
+        try:
+            r = requests.get(
+    		  'https://zenodo.org/api/records',
+    		  params={'q': 'doi:"%s"' % dir})
+        # except (ConnectionError, TimeoutError, MaxRetryError):
+        #     time.sleep(5000)
+            data = r.json()
+            if data['hits']['total'] > 0:
+            	filename = '%s/zenodo-access.json' % dir
+            	new_file = open(filename, "w")
+            	new_file.write(str(data))
+            	new_file.close()
+            elif data['hits']['total'] == 0:
+                filename = '%s/zenodo-access.json' % dir
+                new_file = open(filename, "w")
+                new_file.write('')
+                new_file.close()
+            elif r.status_code != 200:
+                time.sleep(5000)
+        except:
+            with open("db/tmp-zenodo", "a") as myfile:
+                myfile.write("%s" % doi)
     print('Just parsed %d of %d dois through Zenodoo' % (i, len(dirs)))
     i += 1
