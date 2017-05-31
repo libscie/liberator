8fb45a151f3d56099f2c6ec64ffcc938a088716b
CHJ Hartgerink <chjh@protonmail.com>
2017-06-01 00:03:01 +0200

    Bug fix?

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn liberator.wsgi --log-file -
--- liberator/wsgi.py
+++ liberator/wsgi.py
@@ -12,5 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liberator.settings")
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
