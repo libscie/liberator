19d749269abe5d3015d5bb3601bfabe6b6c1f7ad
CHJ Hartgerink <chjh@protonmail.com>
2017-05-31 23:19:04 +0200

    Bug fix

--- liberator/wsgi.py
+++ liberator/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liberator.settings")
-
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
