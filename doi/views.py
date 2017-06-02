2f030133a7dc7234e8233de143592d0a10880549
CHJ Hartgerink <chjh@protonmail.com>
2017-06-02 15:03:39 +0200

    Update template, https

--- doi/views.py
+++ doi/views.py
@@ -3,8 +3,12 @@ from .models import *
 
 def homepage(request):
     count = Article.objects.count()
+    missing = Article.objects.filter(has_been_uploaded = False).count()
+    costs = missing * 30
     article = Article.objects.order_by('?').first()
     return render(request, 'homepage.html', {
         'count': count,
         'article': article,
+        'missing': missing,
+        'costs': costs
     })
---
+++ liberator/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/run/media/chjh/dalek_0001/projects/liberator/liberator/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
