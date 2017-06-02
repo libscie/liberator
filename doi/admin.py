9e4990bdfc49f1a9ee5d42267b75de70d6ed6eb3
CHJ Hartgerink <chjh@protonmail.com>
2017-06-02 16:33:25 +0200

    Update admin.py

--- doi/admin.py
+++ doi/admin.py
@@ -4,8 +4,12 @@ from .models import *
 class InlinePDFAdmin(admin.StackedInline):
     model = PDF
 
+class InlineIllegalAdmin(admin.StackedInline):
+    model = Illegal
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['doi_prefix', 'doi_suffix']
     list_display_links = ['doi_prefix', 'doi_suffix']
     inlines = [InlinePDFAdmin]
+
